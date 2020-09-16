3f8030a0520fc9f3a943a727512bd99e19fa4c96
LittlePinkCookie <alouache.loan@gmail.com>
2020-09-16 17:26:19 +0200

    Fix pseudo

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -2,8 +2,9 @@
   <div class="row">
     <div class="col-6 offset-3 jumbotron align-center">
       <h1 class>Entrez votre pseudo</h1>
-      <input class="form-control mb-3" placeholder="Pseudonyme" />
+      <input class="form-control mb-3" placeholder="Pseudonyme" v-model="pseudo"/>
       <button class="w-100 btn btn-success" v-on:click="test">Go</button>
+      {{ pseudo }}
     </div>
   </div>
 </template>
@@ -11,9 +12,14 @@
 <script>
 export default {
   name: "Home",
+  data: function() {
+    return {
+      pseudo: ''
+    }
+  },
   methods: {
     test: function () {
-      this.$socket.send("aaaaaa");
+      this.$socket.send({ pseudo: this.pseudo });
     },
   },
 };
